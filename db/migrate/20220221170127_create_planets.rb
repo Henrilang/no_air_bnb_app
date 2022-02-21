24381745d3e3e68d314eff0c176a039e9827df21
Henrique Langnas <henri.slang@gmail.com>
2022-02-21 17:05:01 +0000

    schemaUpdate

--- db/migrate/20220221170127_create_planets.rb
+++ db/migrate/20220221170127_create_planets.rb
@@ -1,6 +1,9 @@
 class CreatePlanets < ActiveRecord::Migration[6.1]
   def change
     create_table :planets do |t|
+      t.string :name
+      t.string :location
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
