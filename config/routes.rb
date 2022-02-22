c9200ac07c00b1bf9c2adb0dd3f1da943988bc0f
Alex Taylor <alexhenrytaylor@gmail.com>
2022-02-22 15:10:46 +0000

    updated bookings page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/users/:id/bookings', to: 'users#bookings', as: "bookings"
+  get '/users/:id/account', to: 'users#show', as: "show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
