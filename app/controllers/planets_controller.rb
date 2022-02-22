6e79621e89f47194c87165c154ddf28a6c51b57d
Jake Pople <jakepople@hotmail.co.uk>
2022-02-22 15:07:59 +0000

    planet new/create and new.html.erb created

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,10 +1,23 @@
 class PlanetsController < ApplicationController
-  before_action :set_planet, only: [:show]
+  before_action :set_planet, only: [:show, :new, :create]
   def index
     @planets = Planet.all
   end
 
   def show
+  end
+
+  def new
+    @planet = Planet.new
+  end
+
+  def create
+    @planet = Planet.new(set_params)
+    if @planet.save
+      redirect_to planet_path(@planet)
+    else
+      render :new
+    end
   end
 
   private
