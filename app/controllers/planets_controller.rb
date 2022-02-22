33840f34c2b80e2d67ef9edf90c0aae275ee3fc2
Jake Pople <jakepople@hotmail.co.uk>
2022-02-22 12:25:30 +0000

    commit to pause the branch

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -5,4 +5,17 @@ class PlanetsController < ApplicationController
 
   def show
   end
+
+  def new
+    @planet = planet.find(params[:id])
+    @planet = Planet.new
+  end
+
+  def create
+  end
+
+  private
+
+  def plant_params
+  end
 end
