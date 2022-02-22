2a6201916f63dc097bf43c74fdf02ddbccbe6bb7
alexhtaylor <96840766+alexhtaylor@users.noreply.github.com>
2022-02-22 12:53:43 +0000

    Revert "fixing the models, bookings page and some of the users controller"

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,5 +1,5 @@
 class Planet < ApplicationRecord
-  has_many :users, through: :bookings
+  belongs_to :users_id, through: :bookings
 
   validates :name, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/users/:id/bookings', to: 'users#bookings', as: "bookings"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
